e5ede1d640b97d33b93d016ccdc8326c511a6a0d
Muhammad Haroon <84071543+26haroon26@users.noreply.github.com>
2022-11-23 16:00:00 +0500

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,14 @@
+console.log('I am Haroon');
+import  express  from "express";
+// const express = require('express') ye old hogya he
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    console.log('request ip :', req.ip);
+  res.send('I am Haroon')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
